11fd751222efc347ba07482314e044690fbd061b
ndoua <serge.ndoua@gmail.com>
2018-12-05 12:56:03 +0100

    second commit

---
+++ customers/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    first_name = models.CharField("First name", max_length=225)
+    last_name = models.CharField("last namne", max_length=225)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    createdAt = models.DateTimeField("Creatde At", auto_now_add=True)
+
+    def __str__(self):
+        return self.first_name
